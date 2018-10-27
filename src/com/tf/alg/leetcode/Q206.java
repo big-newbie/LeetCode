c360db9f93b50185f59edc6e552f0fffd9fb50f2
big-newbie <liutingfang_01@foxmail.com>
2018-10-27 23:59:27 +0800

    206

---
+++ src/com/tf/alg/leetcode/Q206.java
@@ -0,0 +1,40 @@
+package com.tf.alg.leetcode;
+
+import com.tf.alg.ListNode;
+
+/**
+ * Created by tingfang
+ * 2018-10-27
+ */
+public class Q206 {
+
+    private static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = null;
+        while (next != null) {
+            ListNode temp = next.next;
+            next.next = head;
+            head = next;
+            next = temp;
+        }
+        return head;
+    }
+
+    private static ListNode reverseList1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode ret = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = ListNode.builder().next(1, 2, 3, 4, 5).build();
+        reverseList1(listNode).print();
+    }
+}
