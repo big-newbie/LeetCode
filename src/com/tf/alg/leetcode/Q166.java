05b8fe48920ce9c75c3d2c8cc40050344810bd1f
big-newbie <liutingfang_01@foxmail.com>
2018-11-01 00:23:30 +0800

    166

--- src/com/tf/alg/leetcode/Q166.java
+++ src/com/tf/alg/leetcode/Q166.java
@@ -1,7 +1,9 @@
 package com.tf.alg.leetcode;
 
+import java.math.BigDecimal;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by tingfang
@@ -9,23 +11,33 @@ import java.util.Map;
  */
 public class Q166 {
     private static String fractionToDecimal(int numerator, int denominator) {
+        return fractionToDecimal1(numerator, denominator);
+    }
+
+    private static String fractionToDecimal1(long numerator, long denominator) {
         if (numerator == 0) {
             return "0";
         }
+        if (numerator < 0 && denominator < 0) {
+            return fractionToDecimal1(-numerator, -denominator);
+        }
+        if (numerator < 0 || denominator < 0) {
+            return "-" + fractionToDecimal1(-numerator, denominator);
+        }
         StringBuilder sb = new StringBuilder();
         sb.append(numerator / denominator);
-        int remainder = numerator % denominator;
+        long remainder = (numerator % denominator);
         if (remainder == 0) {
             return sb.toString();
         }
         StringBuilder fraction = new StringBuilder();
-        Map<Integer, Integer> map = new LinkedHashMap<>();
+        Map<Long, Integer> map = new LinkedHashMap<>();
         int i = 0;
-        while (!map.containsKey(remainder)) {
+        while (!map.containsKey(remainder) && remainder != 0) {
             map.put(remainder, i++);
             remainder *= 10;
             fraction.append(remainder / denominator);
-            remainder = remainder % denominator;
+            remainder = (remainder % denominator);
         }
         sb.append(".");
         if (remainder == 0) {
@@ -42,15 +54,14 @@ public class Q166 {
     }
 
     public static void main(String[] args) {
-        System.out.println(fractionToDecimal(16, 166));
-//        for (int i = 0; i < 100; i++) {
-//            int numerator = new Random().nextInt(1000);
-//            int denominator = new Random().nextInt(1000) + 1;
-//            System.out.println("numerator = " + numerator);
-//            System.out.println("denominator = " + denominator);
-//            System.out.println(fractionToDecimal(numerator, denominator));
-//            System.out.println(numerator / denominator);
-//            System.out.println();
-//        }
+        for (int i = 0; i < 100; i++) {
+            int numerator = new Random().nextInt(100);
+            int denominator = new Random().nextInt(100) + 1;
+            System.out.println("numerator = " + numerator);
+            System.out.println("denominator = " + denominator);
+            System.out.println(fractionToDecimal(numerator, denominator));
+            System.out.println(BigDecimal.valueOf(numerator).divide(BigDecimal.valueOf(denominator), 100, BigDecimal.ROUND_DOWN));
+            System.out.println();
+        }
     }
 }
