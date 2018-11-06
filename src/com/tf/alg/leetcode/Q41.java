e713ad0daed205ad31679e089223ebeb55e5bcca
big-newbie <liutingfang_01@foxmail.com>
2018-11-06 21:19:15 +0800

    41 O(1) space

--- src/com/tf/alg/leetcode/Q41.java
+++ src/com/tf/alg/leetcode/Q41.java
@@ -22,4 +22,38 @@ public class Q41 {
         }
         return nums.length + 1;
     }
+
+    private static int firstMissingPositive(int[] nums) {// O(n) time O(1) space
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] < 0) {
+                nums[i] = len + 1;
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            int prev = nums[i];
+            while (prev > 0 && prev <= len) {
+                int temp = nums[prev - 1];
+                nums[prev - 1] = -1;
+                prev = temp;
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != -1) {
+                return i + 1;
+            }
+        }
+        return len + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("firstMissingPositive(new int[]{1,2,0}) = " + firstMissingPositive(new int[]{1, 2, 0}));
+        System.out.println("firstMissingPositive(new int[]{3,4,-1,1}) = " + firstMissingPositive(new int[]{3, 4, -1, 1}));
+        System.out.println("firstMissingPositive(new int[]{7,8,9,11,12}) = " + firstMissingPositive(new int[]{7, 8, 9, 11, 12}));
+        System.out.println("firstMissingPositive(new int[]{1,2,3,4,5}) = " + firstMissingPositive(new int[]{1, 2, 3, 4, 5}));
+        System.out.println("firstMissingPositive(new int[]{2147483647}) = " + firstMissingPositive(new int[]{2147483647}));
+    }
 }
